72b3b5e2fa25ec54e2e120eaa18e9d70b3bc9293
Linusp <linusp1024@gmail.com>
2016-05-09 14:06:21 +0800

    implemented simple automatic summarization

--- nlp/auto_summary.py
+++ nlp/auto_summary.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import
 
+import sys
+import cPickle
 from itertools import chain
 from collections import Counter
 
@@ -13,7 +15,8 @@ from nlp.utils import (
 )
 
 
-def simple_auto_summary(text, stopwords):
+def simple_auto_summary(text, stopwords=None, number=5):
+    """"""
     # 分句并去除标点
     sents = []
     for sent in sents_tokenize(text):
@@ -21,5 +24,27 @@ def simple_auto_summary(text, stopwords):
         if cur_sent:
             sents.append(cur_sent)
 
-    words = list(chain.from_iterable(sents))
+    words = set(chain.from_iterable(sents))
     term_freqs = Counter(words)
+    stopwords = stopwords or set()
+    for word in words & stopwords:
+        term_freqs.pop(word)
+
+    sents_score = Counter()
+    for i, sent in enumerate(sents):
+        score = sum(term_freqs[word] for word in sent)
+        sents_score[i] = score
+
+    return [sents[i] for i, s in sorted(sents_score.most_common(number), key=lambda x: x[0])]
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print 'Usage: {} <text> <stopfile>'.format(sys.argv[0])
+        sys.exit(1)
+
+    text = open(sys.argv[1]).read().strip()
+    stop = cPickle.load(open(sys.argv[2], 'r'))
+
+    for sent in simple_auto_summary(text, stop, 5):
+        print ''.join(sent)
